a77c5ecb7afaf8388b8268479c8ff50cf83b2a14
emiliechanavat <emilie.chnvt@gmail.com>
2025-02-07 12:40:57 +0100

    Album entity added

--- src/Entity/Album.php
+++ src/Entity/Album.php
@@ -4,5 +4,49 @@ namespace App\Entity;
 
 class Album
 {
+    private $id;
+    private $title;
+    private $auhtor;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuhtor()
+    {
+        return $this->auhtor;
+    }
+
+    /**
+     * @param mixed $auhtor
+     */
+    public function setAuhtor($auhtor): void
+    {
+        $this->auhtor = $auhtor;
+    }
+
 
 }
